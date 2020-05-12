857782e1c750bfff03646e7fc8ca0612ea8f7b80
camilo hernandez <juaca2009@gmail.com>
2020-05-12 18:03:46 -0500

    obtener ips y horarios paciente corregidos

--- api.py
+++ api.py
@@ -98,17 +98,17 @@ def obtener_ips():
     )
     consulta = cursor.fetchall()
     if len(consulta) != 0:
-        temp = list()
-        ips = {}
+        principal = list()
         contador = 0
         while contador < len(consulta):
-            plantilla = {
-                'nombre': consulta[contador][1],
-                'direccion': consulta[contador][2]
+            ips = {
+                'id': consulta[contador][0],
+                'name': consulta[contador][1],
+                'streetAddress': consulta[contador][2]
             }
-            ips[consulta[contador][0]] = plantilla
+            principal.append(ips)
             contador = contador + 1
-        return json.dumps(ips)
+        return json.dumps(principal)
     else:
         return json.dumps({'mensaje':"no hay ips en la base de datos"})
 
@@ -173,7 +173,7 @@ def horarios_paciente(documento):
     global cursor
     cursor.execute(
         """
-        select nro_cita, nro_consultorio, horarios.fecha, ips.nombre, ips.direccion, medicos.nombres, especializaciones.nombre from horarios
+        select nro_cita, horarios.fecha, ips.nombre, ips.direccion, medicos.nombres, especializaciones.nombre from horarios
         inner join consultorios on (horarios.id_consultorio = consultorios.nro_consultorio) 
         inner join ips on (consultorios.id_Ips = ips.id_ips) inner join medicos on (consultorios.id_medico = medicos.nro_documento)
         inner join especializaciones on (especializaciones.id_especializacion = medicos.id_espc)
@@ -183,20 +183,20 @@ def horarios_paciente(documento):
     )
     consulta = cursor.fetchall()
     if len(consulta) != 0:
-        citas = {}
+        citas = list()
         contador = 0
         while contador < len(consulta):
             plantilla = {
-                'fecha': consulta[contador][2].strftime("%d-%b-%Y (%H:%M:%S)"),
-                'ips': consulta[contador][3],
-                'direccion': consulta[contador][4],
-                'medico': consulta[contador][5],
-                'especializacion': consulta[contador][6]
+                'id': consulta[contador][0],
+                'date': consulta[contador][1].strftime("%d-%b-%Y (%H:%M:%S)"),
+                'healthProviderInstitute': consulta[contador][2],
+                'address': consulta[contador][3],
+                'doctorName': consulta[contador][4],
+                'specialization': consulta[contador][5]
             }
-            citas[consulta[contador][0]] = plantilla
+            citas.append(plantilla)
             contador = contador + 1
         return json.dumps(citas)
-
     else:
         return json.dumps({'mensaje': 'no hay citas para este paciente'})
 
